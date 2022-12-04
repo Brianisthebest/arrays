fc415df835d93fd4d0ce78cfd02fc673765bff25
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2022-12-04 15:32:31 -0700

    Add methods and explanations

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,13 @@ strings = ["Sophie", "Reggie", "Galvin", "Andrew"]
 intergers = [32, 30, 29, 34]
 floats = [199.45, 200.25, 99.75, 421.95]
 booleans = [false, true, true, false]
+
+
+# Adds what's being defined in push to the end of the array
+strings.push("Carl")
+# Removes the last element in the array, so 34
+intergers.pop
+# Removes the first element of the array, so 199.45. You can also give a number to remove a specific element
+floats.shift
+# Puts it at the beginning of the array while pushing the rest up in element order
+booleans.unshift(true)
